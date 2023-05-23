5d681e1842285f952b8c394e09863525de556d4b
weidong <1491453343@qq.com>
2023-05-23 11:43:47 +0800

    1.添加shell脚本根据传入的参数编译不同板型uboot功能

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export ARCH=arm
-#export CROSS_COMPILE=/opt/poky/1.7/sysroots/x86_64-pokysdk-linux/usr/bin/arm-poky-linux-gnueabi/arm-poky-linux-gnueabi-
-export CROSS_COMPILE=/opt/gcc-linaro-4.9-2016.02-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+export CROSS_COMPILE=/opt/poky/1.7/sysroots/x86_64-pokysdk-linux/usr/bin/arm-poky-linux-gnueabi/arm-poky-linux-gnueabi-
+
 build_uboot(){
     make distclean
     rm $OBJ -f
@@ -15,11 +15,21 @@ if [[ $1 == "clean" ]]; then
      rm u-boot-M6708U-512LI.imx u-boot-M6708U-1GLI.imx u-boot-M6708Q-1GLI.imx -f
      exit 0 
 else
-#	echo "build M6708U-512LI-T u-boot.imx"
-#	sed -i "s/MX6DL,DEFAULT_FDT_FILE=\"imx6dl-sabresd.dtb\",DDR_MB=1024,SYS_USE_SPINOR /MX6DL,DEFAULT_FDT_FILE=\"imx6dl-sabresd.dtb\",DDR_MB=512,SYS_USE_SPINOR /g" boards.cfg
-#	CONFIG=mx6dlsabresd_config
-#	OBJ=u-boot-M6708U-512LI.imx
-#	build_uboot
+
+	if [[ $1 == "k256host" ]]; then
+		cp mx6sabre_common_k256host.h ./include/configs/mx6sabre_common.h
+	elif [[ $1 == "k256monitor" ]]; then
+		cp mx6sabre_common_k256monitor.h ./include/configs/mx6sabre_common.h
+	elif [[ $1 == "ktc101max" ]]; then
+		cp mx6sabre_common_ktc101max.h ./include/configs/mx6sabre_common.h
+	elif [[ $1 == "tk200_ktc101" ]]; then
+		cp mx6sabre_common_tk200_ktc101.h ./include/configs/mx6sabre_common.h
+	elif [[ $1 == "k256zhongji" ]]; then
+		cp mx6sabre_common_k256zhongji.h ./include/configs/mx6sabre_common.h
+	else
+		cp mx6sabre_common.h ./include/configs/mx6sabre_common.h
+	fi
+
 
 	echo "build M6708U-1GLI-T u-boot.imx";
 	sed -i "s/MX6DL,DEFAULT_FDT_FILE=\"imx6dl-sabresd.dtb\",DDR_MB=512,SYS_USE_SPINOR /MX6DL,DEFAULT_FDT_FILE=\"imx6dl-sabresd.dtb\",DDR_MB=1024,SYS_USE_SPINOR /g" boards.cfg
@@ -27,11 +37,5 @@ else
 	OBJ=u-boot-M6708U-1GLI.imx
 	build_uboot
 
-#	echo "build M6708Q-1GLI-T/M6708Q-1GLC-T u-boot.imx"
-#	CONFIG=mx6qsabresd_config
-#	OBJ=u-boot-M6708Q-1GLC.imx
-#	build_uboot
-
-#    mv ../u-boot-M6708U-512LI.imx ../u-boot-M6708U-1GLI.imx ../u-boot-M6708Q-1GLC.imx . -f
 fi
 
